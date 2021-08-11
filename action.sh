645a53a15956088e10cdde6719976f748678cc5c
DunggVN <65992011+DunggVN@users.noreply.github.com>
2021-08-11 18:05:16 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -2,3 +2,5 @@ branch=master
 git clone -b $branch http://github.com/HiHelloIII/KairoKangedBot /root/KairoBot
 cp kairobot/config.ini /root/KairoBot/config.ini
 cd /root/KairoBot
+pip3 install -U -r requirements.txt
+python3 -m tg_bot
